94e94f940b9cd05c4b255560cdf3c85869512b42
uridr <orioldomingoroig@gmail.com>
2022-02-19 09:44:43 +0100

    pep3 syntax. gitignore updated. information step working

--- collection/information/utils.py
+++ collection/information/utils.py
@@ -3,16 +3,16 @@ from SPARQLWrapper import SPARQLWrapper, JSON
 import json
 import sys
 
-def readJSON(file):
+def read_json(file):
     with open(file) as f:
         json_file = json.load(f)
     return json_file
 
-def writeJSON2File(file, data):
+def write_json_2_file(file, data):
 	with open(file, 'w', encoding='utf8') as outfile:
 		json.dump(data, outfile, ensure_ascii=False)
 
-def getResults(endpoint_url, query):
+def get_results(endpoint_url, query):
     user_agent = "WDQS-example Python/%s.%s" % (sys.version_info[0], sys.version_info[1])
     # TODO adjust user agent; see https://w.wiki/CX6
     sparql = SPARQLWrapper(endpoint_url, agent=user_agent)
